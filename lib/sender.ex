58d82fb6c1ce2127fd3f440ced7966d5ca32adb2
marcin piotr miszczyk <mpmiszczyk@gmail.com>
2021-12-26 18:40:23 +0100

    Template for costly sending emails.

---
+++ lib/sender.ex
@@ -0,0 +1,20 @@
+defmodule Sender do
+  @moduledoc false
+
+  @one_second 1000
+
+  require Logger
+
+  def send_email(email) do
+    Process.sleep(around_seconds(3))
+    Logger.info("Sent email", email: email)
+    {:ok, :email_sent}
+  end
+
+  defp around_seconds(seconds, miliseconds_variance \\ 300) do
+    seconds * @one_second + variance(miliseconds_variance)
+  end
+
+  defp variance(miliseconds_variance),
+    do: :rand.uniform(miliseconds_variance) * Enum.random([1, -1])
+end
